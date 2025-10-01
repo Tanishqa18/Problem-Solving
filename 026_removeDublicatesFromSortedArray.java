b3d08366fb4304e6e6d101a4418f798e7682d7f0
tanishqa <tanishqa.kumavat05@gmail.com>
2025-10-01 06:28:19 +0530

    Create 026_removeDublicatesFromSortedArray.java

---
+++ 026_removeDublicatesFromSortedArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] num) {
+        if (num.length == 0) return 0;
+        int k = 1;
+        for (int i = 1; i<num.length; i++){
+            if(num[i] != num[i-1]){
+                num[k] = num[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
