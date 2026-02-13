519e2c82b427882a1c8cf5b546d84898acab9bbe
tanishqa <tanishqa.kumavat05@gmail.com>
2026-02-13 05:54:24 +0530

    Implement largest rectangle in histogram solution

---
+++ 084_largestRectangleInHistogram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] stack = new int[n + 1];
+        int top = -1;
+        int max = 0;
+
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n) ? 0 : heights[i];
+
+            while (top != -1 && heights[stack[top]] > h) {
+                int height = heights[stack[top--]];
+                int width = (top == -1) ? i : i - stack[top] - 1;
+                max = Math.max(max, height * width);
+            }
+
+            stack[++top] = i;
+        }
+
+        return max;
+    }
+}
