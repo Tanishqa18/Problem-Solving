fbc31c2cfad616a49db1e8fb936f9ab22ab67652
tanishqa <tanishqa.kumavat05@gmail.com>
2026-02-25 10:43:48 +0530

    Create 1071_greatestCommonDivisorOfString.java

---
+++ 1071_greatestCommonDivisorOfString.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1 + str2).equals(str2 + str1)){
+            return "";
+        }
+        int len = gdc(str1.length(), str2.length());
+        return str1.substring(0, len);
+    }
+    private int gdc(int a, int b){
+        while(b != 0){
+            int temp = a%b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
