9cf507b72adc96664f9246453b89a84ac5de6b09
tanishqa <tanishqa.kumavat05@gmail.com>
2026-04-06 07:26:06 +0530

    Create 2300_successivepairofSpellsandPortion.java

---
+++ 2300_successivepairofSpellsandPortion.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int n = spells.length, m = potions.length;
+
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int spell = spells[i];
+
+            long need = (success + spell - 1) / spell; 
+
+            int left = 0, right = m - 1;
+            int index = m;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (potions[mid] >= need) {
+                    index = mid;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            result[i] = m - index;
+        }
+
+        return result;
+    }
+}
