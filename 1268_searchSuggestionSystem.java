c2ea6dd76a1d1bcba4b476856a75e9ab277346bf
tanishqa <tanishqa.kumavat05@gmail.com>
2026-04-16 12:28:33 +0530

    Update 1268_searchSuggestionSystem.java

--- 1268_searchSuggestionSystem.java
+++ 1268_searchSuggestionSystem.java
@@ -1,1 +1,29 @@
 
+import java.util.*;
+
+class Solution {
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        List<List<String>> result = new ArrayList<>();
+        
+        Arrays.sort(products); 
+        
+        String prefix = "";
+        
+        for (char ch : searchWord.toCharArray()) {
+            prefix += ch;
+            
+            List<String> temp = new ArrayList<>();
+            
+            for (String product : products) {
+                if (product.startsWith(prefix)) {
+                    temp.add(product);
+                    if (temp.size() == 3) break; 
+                }
+            }
+            
+            result.add(temp);
+        }
+        
+        return result;
+    }
+}
