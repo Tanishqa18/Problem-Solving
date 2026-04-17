23e4c550504ab3345c822fa2b71cb68803a7554b
tanishqa <tanishqa.kumavat05@gmail.com>
2026-04-17 08:32:58 +0530

    Add graph cloning functionality
    Implement depth-first search to clone a graph.

---
+++ 133_cloneGraph.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    
+    private HashMap<Node, Node> map = new HashMap<>();
+    
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        return dfs(node);
+    }
+    
+    private Node dfs(Node node) {
+    
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+   
+        Node clone = new Node(node.val);
+        map.put(node, clone);
+        
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(dfs(neighbor));
+        }
+        
+        return clone;
+    }
+}
