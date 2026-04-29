9b58896c352c31cb95319e143cab18b6adc5184b
tanishqa <tanishqa.kumavat05@gmail.com>
2026-04-29 06:56:31 +0530

    Implement Pacific Atlantic Water Flow algorithm

---
+++ 417_pacificAtlanticWaterFlow.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            dfs(heights, pacific, i, 0);
+            dfs(heights, atlantic, i, n - 1);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(heights, pacific, 0, j);
+            dfs(heights, atlantic, m - 1, j);
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    result.add(Arrays.asList(i, j));
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private void dfs(int[][] heights, boolean[][] visited, int row, int col) {
+        visited[row][col] = true;
+
+        int[][] directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+        for (int[] dir : directions) {
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+
+            if (newRow >= 0 && newCol >= 0 &&
+                newRow < heights.length && newCol < heights[0].length &&
+                !visited[newRow][newCol] &&
+                heights[newRow][newCol] >= heights[row][col]) {
+
+                dfs(heights, visited, newRow, newCol);
+            }
+        }
+    }
+}
