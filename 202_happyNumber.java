8a9c50e45737799fc92f8698b4da3ba0686f7185
tanishqa <tanishqa.kumavat05@gmail.com>
2026-06-24 10:52:42 +0530

    Update 202_happyNumber.java

--- 202_happyNumber.java
+++ 202_happyNumber.java
@@ -1,18 +1,24 @@
 class Solution {
+
     public boolean isHappy(int n) {
+
         HashSet<Integer> set = new HashSet<>();
-        while(n!=1 && !set.contains(n)){
+
+        while(n != 1 && !set.contains(n)) {
+
             set.add(n);
-            n=getsum(n);
+
+            int sum = 0;
+
+            while(n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+            n = sum;
         }
-        return n==1;    
-    }private int getsum(int n){
-        int sum =0;
-        while(n>0){
-            int digit =n%=10;
-            sum+= digit * digit;;
-            n/=10;
-        }
-        return sum;
+
+        return n == 1;
     }
 }
