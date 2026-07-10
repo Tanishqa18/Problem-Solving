dc24bb6d1ee9ac7dc1440bdac70840f61af9954c
tanishqa <tanishqa.kumavat05@gmail.com>
2026-07-10 14:23:06 +0530

    Implement QuadTree construction from grid

---
+++ 427_constructQuadTree.java
@@ -0,0 +1,64 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+    public Node() {}
+
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+    }
+
+    public Node(boolean val, boolean isLeaf,
+                Node topLeft, Node topRight,
+                Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
+*/
+
+class Solution {
+    public Node construct(int[][] grid) {
+        return build(grid, 0, 0, grid.length);
+    }
+
+    private Node build(int[][] grid, int row, int col, int size) {
+        if (isSame(grid, row, col, size)) {
+            return new Node(grid[row][col] == 1, true);
+        }
+
+        int half = size / 2;
+
+        Node topLeft = build(grid, row, col, half);
+        Node topRight = build(grid, row, col + half, half);
+        Node bottomLeft = build(grid, row + half, col, half);
+        Node bottomRight = build(grid, row + half, col + half, half);
+
+        return new Node(true, false, topLeft, topRight, bottomLeft, bottomRight);
+    }
+
+    private boolean isSame(int[][] grid, int row, int col, int size) {
+        int value = grid[row][col];
+
+        for (int i = row; i < row + size; i++) {
+            for (int j = col; j < col + size; j++) {
+                if (grid[i][j] != value) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
